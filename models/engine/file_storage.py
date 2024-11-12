bf5b42f1d4768d20487e26bf97e7a4fdb2fc9465
lensonlovescode <lensonisinparis@gmail.com>
2024-11-12 21:36:42 +0300

    File Storage class complete

---
+++ models/engine/file_storage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This module defines a class FileStorage that
+serializes instances to a JSON file and deserializes JSON file to instances
+"""
+import json
+
+
+class FileStorage():
+    """
+    This class serializes instances to
+    a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        returns the dictionary __objects
+        """
+        return (FileStorage.__objects)
+
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = obj.__class__.__name__ + . + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        dict_objects = {key: value.to_dict() for
+                        key, value in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w') as f:
+            f.write(json.dumps(dict_objects))
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists
+        otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised
+        """
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                dict_objs = json.loads(f.read())
+                for key, value_dict in dict_objs.items():
+                    class_name = value_dict.pop('__class__')
+                    obj = eval(f"{class_name}(**value_dict)")
+                    self.new(obj)
+        except FileNotFoundError:
+            pass
