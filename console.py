9576789e4c44b09ff71cd8c80de0b53e19a4e761
FionaGachuuri <fionairuka@gmail.com>
2024-11-16 03:26:29 +0300

    Add destroy, all, and update methods to console.py.

--- console.py
+++ console.py
@@ -16,7 +16,15 @@ class HBNBCommand(cmd.Cmd):
     This class provides command-line interface commands to create, retrieve,
     update, and delete instances of classes within the Airbnb project.
     """
-    prompt = "(hbnb) "
+    prompt = "(hbnb)"
+    valid_classes = ("BaseModel",
+                     "User",
+                     "Place",
+                     "State",
+                     "City",
+                     "Amenity",
+                     "Review"
+                     )
 
     def do_greet(self, arg):
         """
@@ -76,7 +84,7 @@ class HBNBCommand(cmd.Cmd):
         if len(args) < 1:
             print("** class name missing **")
             return
-        if len(args) == 1:
+        if len(args) < 2:
             print("** instance id missing **")
             return
 
@@ -95,6 +103,102 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
             return
         print(obj.__str__())
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id.
+        Then saves the changes into the JSON file.
+        """
+        args = arg.split()
+        if len(args) < 1:
+            print("** class name missing **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+
+        class_name, obj_id = args
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        objs = storage.all()
+        full_key = f"{class_name}.{obj_id}"
+        if full_key not in objs:
+            print("** no instance found **")
+            return
+
+        del objs[full_key]
+        storage.save()
+
+    def do_all(self, arg):
+        """
+        prints all string representation of all instances based or
+        not on the class name.
+        """
+        args = arg.split()
+        objs = storage.all()
+
+        if args:
+            class_name = args[0]
+            if class_name not in self.valid_classes:
+                print("** class doesn't exist **")
+                return
+            print(
+                [
+                    str(obj)
+                    for key, obj in objs.items()
+                    if key.startswith(class_name)
+                ]
+            )
+        else:
+            print([str(obj) for obj in objs.values()])
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and
+        id by adding or updating attribute,
+        and saves the changes into the JSON file.
+        """
+        args = arg.split()
+        if len(args) < 1:
+            print("** class name missinf **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        if len(args) == 3:
+            print("** value missing **")
+            return
+
+        class_name, obj_id, attr_name, attr_value = args[:4]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        objs = storage.all()
+        full_key = f"{class_name}.{obj_id}"
+        obj = objs.get(full_key)
+        if obj is None:
+            print("** no instance found **")
+            return
+
+        try:
+            if attr_value.isdigit():
+                attr_value = int(attr_value)
+            elif '.' in attr_value:
+                attr_value = float(attr_value)
+            else:
+                attr_value = attr_value.strip('"').strip("'")
+        except ValueError:
+            pass
+
+        if attr_name not in ['id,' 'created_at', 'updated_at']:
+            setattr(obj, attr_name, attr_value)
+            obj.save()
 
 
 if __name__ == '__main__':
