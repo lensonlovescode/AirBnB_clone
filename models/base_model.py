41e816e515377ff01ffc77296eee4e895ad70edc
lensonlovescode <lensonisinparis@gmail.com>
2024-11-17 20:07:53 +0300

    class should not be added from kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -32,10 +32,11 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.fromisoformat(value))
-                else:
-                    setattr(self, key, value)
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
