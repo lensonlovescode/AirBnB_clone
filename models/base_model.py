383c673790446979197461b048c46cf642d967e7
lensonlovescode <lensonisinparis@gmail.com>
2024-11-17 21:16:40 +0300

    checker

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,11 @@ class BaseModel:
                         setattr(self, key, datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+                self.updated_at = self.created_at
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
